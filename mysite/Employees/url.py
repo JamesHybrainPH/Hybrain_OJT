2fd06d3cc95a3baccf26c3976a98b935dd0922c3
James Jumilla <128009666+JamesHybrainPH@users.noreply.github.com>
2023-04-14 14:14:44 +0800

    Update url.py

--- mysite/Employees/url.py
+++ mysite/Employees/url.py
@@ -1,4 +1,9 @@
-from .views import CreateEmployee, EmployeeList, EmployeeDetail, EmployeesSearch, EmployeesRegularization , DeleteEmployee , EditEmployee , WorkSchedulesView , WorkScheduleCreateView , EmployeeView
+from .views import (
+    CreateEmployee, EmployeeList, EmployeeDetail, 
+    EmployeesSearch, EmployeesRegularization, 
+    DeleteEmployee, EditEmployee, 
+    WorkSchedulesView,   EmployeeView
+)
 from django.urls import path 
 from . import views
 
@@ -11,7 +16,7 @@ urlpatterns = [
     path('employees/delete/<int:pk>/', DeleteEmployee.as_view(), name='delete-employee'),
     path('employees/edit/<int:pk>/', EditEmployee.as_view(), name='edit_employee'),
     path('workschedules/', WorkSchedulesView.as_view()),
-    path('workschedules/create/', WorkScheduleCreateView.as_view(), name='workschedule-create'),
+    path('workschedules/create/', WorkSchedulesView.as_view(), name='workschedule-create'),
     path('workschedules/', WorkSchedulesView.as_view(), name='workschedules'),
     path('employees/<int:employee_id>/', EmployeeView.as_view(), name='employee-detail'),
     ]
